c2e5a8d5905b74796417987580287a7d3f7f1051
KD1601 <khanhduy8768@gmail.com>
2023-04-01 17:50:31 +0700

    Done sum bill

--- src/controllers/manager.controller.js
+++ src/controllers/manager.controller.js
@@ -39,7 +39,9 @@ async function toHandleSumBillPage(req, res, next) {
 
 async function toHandleBillHistoryPage(req, res, next) {
     try {
-        res.render('manager-bill-history')
+        const bills = await managerServices.getBills()
+        bills
+        // res.render('manager-bill-history',{bills: bills})
     } catch (err) {
         console.error('An error when direct to manager-bill-history page', err.message);
         next(err);
@@ -48,8 +50,31 @@ async function toHandleBillHistoryPage(req, res, next) {
 
 async function toHandleDetailOfBillPage(req, res, next) {
     try {
-        if (req.params.id == "DH0101") {
-            res.render('detail-order', { id: req.params.id });
+        if (req.params.id) {
+            const detail = await homeServices.getBillDetail(req.params.id)
+            const cacMaMonAn = detail.map(item => item.mamonan);
+            async function getFoods(callback) {
+                const promises = cacMaMonAn.map(async (element) => {
+                    let food = await homeServices.getFood(element);
+                    return food;
+                });
+                arrFood = await Promise.all(promises);
+                callback(arrFood);
+            }
+
+            getFoods((arrFood) => {
+                arrFood.flat().forEach(item2 => {
+                    const item1 = detail.find(item1 => item1.mamonan === item2.mamonan);
+                    if (item1) {
+                        item2.soluong = item1.soluong;
+                        item2.ghichu = item1.ghichu;
+                    }
+                });
+                const arr = arrFood.flat()
+                // console.log(arr)
+                res.render('detail-order', {data: arr})
+            });
+            
         }
     } catch (err) {
         console.error('An error when direct to detail-order page', err.message);
